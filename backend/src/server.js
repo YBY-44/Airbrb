1a5428d80da6fefc37f5aa2a3dcd277b438f1325
yby <yuby0404_qd@163.com>
2024-09-22 13:33:29 +1000

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -41,10 +41,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(morgan(':method :url :status'));
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://boyang-airbrb.netlify.app');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
-
 const catchErrors = (fn) => async (req, res) => {
   try {
     console.log(`Authorization header is ${req.header('Authorization')}`);
